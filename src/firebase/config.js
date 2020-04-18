4757fcb3390a4f2b4ee683b0ccadc98f4f3cb1c6
jaymark <jaykmark@gmail.com>
2020-04-18 16:44:51 -0700

    Setup Firebase.

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCOX7_N842IhdemPYDhITotI0AFeMVAdlI",
+  authDomain: "newsfeed-33209.firebaseapp.com",
+  databaseURL: "https://newsfeed-33209.firebaseio.com",
+  projectId: "newsfeed-33209",
+  storageBucket: "newsfeed-33209.appspot.com",
+  messagingSenderId: "614799942736",
+  appId: "1:614799942736:web:7f9c10e62a4f05ba3edd55"
+};
+
+export default firebaseConfig;
