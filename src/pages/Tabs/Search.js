374b618494535e52db0e0aa8076bc5af3dbe4013
jaymark <jaykmark@gmail.com>
2020-04-18 16:09:34 -0700

    Create initial Routes and Tabs.

---
+++ src/pages/Tabs/Search.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { IonPage, IonHeader, IonToolbar, IonTitle } from '@ionic/react';
+
+function Search() {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Search</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+    </IonPage>
+  );
+};
+
+export default Search;
